3fa919124513feb88b43fba5e2950368ef368638
puter.zhang <zlydwyyx@126.com>
2018-08-31 18:23:55 +0800

    Initial commit

---
+++ app/src/main/java/com/kotlin/douban/demo/activity/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.kotlin.douban.demo.activity
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+@SuppressLint("Registered")
+abstract class BaseActivity: AppCompatActivity() {
+    private lateinit var mContext: Context
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getResId())
+        mContext = this
+        initView()
+        initData()
+    }
+
+    open fun initView() {}
+
+    open fun initData() {}
+
+    abstract fun getResId():Int
+}
---
+++ app/src/main/java/com/kotlin/douban/demo/activity/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.kotlin.douban.demo.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import com.kotlin.douban.demo.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun getResId(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+
+        /*fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }*/
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+           /* R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }*/
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
